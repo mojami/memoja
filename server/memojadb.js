b89e06987db7cea8ab5275510475bcc515afc325
maku77 <maku@makus-MacBook-Air.local>
2014-05-05 02:41:04 +0900

    Add apis for tags (/api/tags).

--- server/memojadb.js
+++ server/memojadb.js
@@ -1,16 +1,6 @@
 var mongoose = require('mongoose');
 var DB_URL = 'mongodb://localhost/memojadb';
 
-// Create 'Memo' model.
-var MemoSchema = new mongoose.Schema({
-    title: String,
-    content: String,
-    created: { type: Date, default: Date.now },
-    updated: { type: Date, default: Date.now }
-});
-
-var Memo = mongoose.model('Memo', MemoSchema);
-
 // If we cannot connect to the mongo DB, the process exits.
 mongoose.connection.on('error', function (err) {
     console.log('ERROR: Failed to connect to MongoDB');
@@ -21,15 +11,41 @@ mongoose.connection.on('error', function (err) {
 // Connect to the DB.
 mongoose.connect(DB_URL);
 
-// Create sample data if there is no data in DB.
-Memo.count(function (err, count) {
-    if (count > 0) {
-        return;
-    }
-    Memo.create({title: 'Title 1', content: 'Content 1'}, function() {});
-    Memo.create({title: 'Title 2', content: 'Content 2'}, function() {});
-    Memo.create({title: 'Title 3', content: 'Content 3'}, function() {});
+// Create 'Memo' model.
+var MemoSchema = new mongoose.Schema({
+    title: String,
+    content: String,
+    tags: [String],
+    created: { type: Date, default: Date.now },
+    updated: { type: Date, default: Date.now }
 });
+var Memo = mongoose.model('Memo', MemoSchema);
+
+// Create 'Tag' model.
+var TagSchema = new mongoose.Schema({
+    name: String
+});
+var Tag = mongoose.model('Tag', TagSchema);
+
+// Create sample data if there is no data in DB.
+//Memo.remove({}, function (err, count) {
+    Memo.count(function (err, count) {
+        if (count > 0) { return; }
+        Memo.create({title: 'Title 1', content: 'Content 1', tags:['TagA', 'TagB']}, function() {});
+        Memo.create({title: 'Title 2', content: 'Content 2', tags:['TagB', 'TagC']}, function() {});
+        Memo.create({title: 'Title 3', content: 'Content 3', tags:['TagC', 'TagA']}, function() {});
+    });
+//});
+//Tag.remove({}, function (err, count) {
+    Tag.count(function (err, count) {
+        if (count > 0) { return; }
+        Tag.create({name: 'Tag A'});
+        Tag.create({name: 'Tag B'});
+        Tag.create({name: 'Tag C'});
+        Tag.create({name: 'Tag D'});
+    });
+//});
 
 // Export interfaces.
 exports.Memo = Memo;
+exports.Tag = Tag;
--- server/routes/tags.js
+++ server/routes/tags.js
@@ -1,32 +1,56 @@
+/**
+ * A route definition for /tags.
+ */
+var memojadb = require('../memojadb');
+
 // CREATE
-exports.createTag = function(req, res) {
-    // TODO: create a new tag
-    res.send('New tag has been created.');
+exports.createTag = function (req, res) {
+    console.log('[API] Tag.createTag');
+    var tag = req.body;
+    memojadb.Tag.create(tag, function (err) {
+        if (err) {
+            res.json({error: err.message});
+            return;
+        }
+        res.json({message: 'A new tag has been created'});
+    });
 };
 
 // READ (all)
-exports.readAllTags = function(req, res) {
-    res.json([
-        {
-            id: 'tag_id1',
-            name: 'Tag1'
-        },
-        {
-            id: 'tag_id2',
-            name: 'Tag2'
-        },
-    ]);
+exports.readAllTags = function (req, res) {
+    console.log('[API] Tag.readAllTags');
+    memojadb.Tag.find(function (err, tags) {
+        if (err) {
+            res.json({error: err.message});
+            return;
+        }
+        res.json(tags);
+    });
 };
 
 // UPDATE
-exports.updateTag = function(req, res) {
-    // TODO: modify a tag
-    res.send('The tag has been modified');
+exports.updateTag = function (req, res) {
+    var id = req.params.id;
+    var tag = req.body;
+    console.log('[API] Tag.updateTag:', id);
+    memojadb.Tag.findByIdAndUpdate(id, tag, function (err) {
+        if (err) {
+            res.json({error: err.message});
+            return;
+        }
+        res.json({message: 'The tag has been updated'});
+    });
 };
 
 // DELETE
-exports.deleteTag = function(req, res) {
-    // TODO: delete a tag
-    res.send('The tag has been deleted');
+exports.deleteTag = function (req, res) {
+    var id = req.params.id;
+    console.log('[API] Tag.deleteTag:', id);
+    memojadb.Tag.findOneAndRemove({_id: id}, function (err) {
+        if (err) {
+            res.json({error: err.message});
+            return;
+        }
+        res.json({message: 'The tag has been deleted'});
+    });
 };
-
